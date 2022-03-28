a58d4dd9d0469f829a7cf82a6a426b61804c69c7
Sylamsh <amrutasrisvara@gmail.com>
2022-03-28 22:00:16 +0530

    callback implemented

--- index.js
+++ index.js
@@ -5,8 +5,24 @@ const app = express()
 app.use(express.json({ extended : true }))
 app.use(express.urlencoded({ extended : true }))
 
+
 app.get('/', (req, res) => {
-    res.send('Insta Minutes Assignment')
+    const date = new Date
+    res.send(date.toISOString())
+})
+
+const callback = (arr, idx) => {
+    if(idx >= arr.length)
+        return
+    setTimeout(()=>{
+        console.log(arr[idx].text.split('').reverse().join('') + ' ' + arr[idx].text.length + 's')
+        callback(arr, idx+1)
+    }, arr[idx].text.length * 1000)
+}
+
+app.post('/post', (req, res) => {
+    callback(req.body.events, 0)
+    res.send("check the console")
 })
 
 const PORT = process.env.PORT || 3350
